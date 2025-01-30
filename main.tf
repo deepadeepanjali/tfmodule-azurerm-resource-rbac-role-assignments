e8cde7650f28015215caf3723b06fe29ebaaf698
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-30 15:00:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_assignment" "role_assign" {
-  for_each    = var.role_assignments
-  principal_id       = each.value.principal_id
-  scope = each.value.scope
-  role_definition_name = each.value.role_definition_name
-  }
+  for_each             = var.role_assignments
+  principal_id         = each.value.principal_id
+  scope                = each.value.scope
+  role_definition_name = each.value.role
+}
