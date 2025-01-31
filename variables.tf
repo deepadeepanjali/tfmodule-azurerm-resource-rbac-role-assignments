fb004809e7c1293e7d4b97555037e8e2330dafe9
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-31 21:27:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,8 @@
-#variable "role_assignments" {
- # type = map(object({
- #   principal_id = string
- #   scope        = string
-  #  role         = string
-  #}))
- # description = "Map of roles assignment, and scope"
-#}
-
-
-variable "principal_id" {
-  description = "principal_id"
-  type        = string
-}
-variable "scope" {
-  description = "scope"
-  type        = string
-}
-variable "role" {
-  description = "role"
-  type        = string
+variable "role_assignments" {
+  type = map(object({
+    principal_id = string
+    scope        = string
+    role         = string
+  }))
+  description = "Map of roles assignment, and scope"
 }
